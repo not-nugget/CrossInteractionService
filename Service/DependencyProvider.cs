48290f2bfb921c93c8e21d211da52d5ba56f75bf
whosNugget <19211255+whosNugget@users.noreply.github.com>
2021-11-20 01:09:24 -0500

    Update DependencyProvider.cs

--- Service/DependencyProvider.cs
+++ Service/DependencyProvider.cs
@@ -10,7 +10,7 @@ namespace CrossInteractionService.Service
 
     public class DependencyProvider : IServiceProvider
     {
-        private readonly SortedDictionary<Type, object> services;
+        private readonly Dictionary<Type, object> services;
         private readonly IServiceProvider rootProvider;
         private readonly bool hasRootProvider = false;
 
@@ -19,7 +19,7 @@ namespace CrossInteractionService.Service
             this.rootProvider = rootProvider;
             hasRootProvider = this.rootProvider is not null;
         }
-        public DependencyProvider(IServiceProvider rootProvider, SortedDictionary<Type, object> services) : this(rootProvider)
+        public DependencyProvider(IServiceProvider rootProvider,  Dictionary<Type, object> services) : this(rootProvider)
         {
             this.services = new(services);
         }
@@ -37,7 +37,7 @@ namespace CrossInteractionService.Service
 
     public class DependencyProviderBuilder
     {
-        private readonly SortedDictionary<Type, object> services = new();
+        private readonly Dictionary<Type, object> services = new();
         IServiceProvider rootProvider = null;
 
         public DependencyProviderBuilder AddRootProvider(IServiceProvider rootProvider)
